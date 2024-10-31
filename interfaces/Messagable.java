bb5be2efd6337f2b0004bc35ab2734d636ec825a
Yliu-101 <kailiu989@gmail.com>
2024-10-31 13:36:10 -0400

    Implemented majority of Message class and interface

--- interfaces/Messagable.java
+++ interfaces/Messagable.java
@@ -3,13 +3,17 @@ import src.User;
 
 import java.util.Date;
 public interface Messagable {
+    String MESSAGE_SEP = "Hyvp1VlujMNPHY9nySd25fxaluLUrI";
+    String CONVO_END = "NIIztzeaegR12UzH1ra01BhKjQbHMH";
     // will have fields of user
     String toString();
     String getFileName();
     void setFileName(String fileName);
     void pushToDatabase();
-    User getUser();
-    void setUser(User user);
+    User getSender();
+    void setSender(User user);
+    User getReceiver();
+    void setReceiver(User user);
     Date getDate();
     void setDate(Date date);
 
--- src/Message.java
+++ src/Message.java
@@ -9,43 +9,92 @@ public class Message implements Messagable {
     User receiver;
     String fileName;
     Date date;
+
     public Message(User sender, User receiver, Date date) {
         this.sender = sender;
         this.receiver = receiver;
         this.date = date;
+        if (sender.getName().compareTo(receiver.getName()) < 0) {
+            fileName = sender.getName() + "@" + receiver.getName();
+        } else {
+            fileName = receiver.getName() + "@" + sender.getName();
+        }
     }
+    public Message(){
+
+    }
+
+    /**
+     * @return The file path that the message is stored within
+     */
     @Override
     public String getFileName() {
-        return "";
+        return fileName;
     }
 
+    /**
+     * sets the filename (path) of the place where the message is stored to a new filename
+     */
     @Override
     public void setFileName(String fileName) {
-
+        this.fileName = fileName;
     }
 
+    /**
+     * Pushes the message to the file of [filename] with the proper separators in place
+     *
+     */
     @Override
     public void pushToDatabase() {
 
     }
 
+    /**
+     * @return the User object representing the sender of the message
+     */
     @Override
-    public User getUser() {
-        return null;
+    public User getSender() {
+        return this.sender;
     }
 
+    /**
+     * @param user
+     */
     @Override
-    public void setUser(User user) {
-
+    public void setSender(User user) {
+        this.sender = user;
     }
 
+    /**
+     * @return the receiver who received the message
+     */
+    @Override
+    public User getReceiver() {
+        return this.receiver;
+    }
+
+    /**
+     * @param user sets the receiver to a new user
+     */
+    @Override
+    public void setReceiver(User user) {
+        this.receiver = user;
+    }
+
+    /**
+     * @return date of the message
+     */
     @Override
     public Date getDate() {
-        return null;
+        return date;
     }
 
+    /**
+     * @param date sets the date that the message was sent to custom date
+     */
     @Override
     public void setDate(Date date) {
-
+        this.date = date;
     }
+
 }
