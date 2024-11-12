0cebac2a7f575b3e4acf81065607a374ce3b6886
FertileJungle91 <one.giant.cowboy@gmail.com>
2024-11-12 17:14:21 -0500

    Finished user signup and login of server implementation

--- src/ServerImplementation.java
+++ src/ServerImplementation.java
@@ -2,7 +2,6 @@ package src;
 
 import interfaces.Server;
 import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -10,10 +9,19 @@ import java.util.HashMap;
 
 public class ServerImplementation implements Server {
     public ServerSocket serverSocket;
-    public ArrayList<String> currentUsers;
-    public HashMap<String, Socket> userNetMap;
-    private final Thread updateUserThread;
+    public ArrayList<Socket> userSockets;
+    private ArrayList<ConversersationHandler> activeConversations;
 
+    public static HashMap<String, Socket> userNetMap;
+    public static ArrayList<User> activeUsers = new ArrayList<>();
+
+    private final Thread userSocketUpdate;
+
+    public final byte PACKET_OPEN = 0x2;
+    public final byte PACKET_CLOSE = 0x3;
+    public final byte SEPERATOR = 0x4;
+
+    //Reading in message data from client, including recipient name, and message content.
     private ServerImplementation(int portnumber) {
         try{
             serverSocket = new ServerSocket(portnumber);
@@ -21,17 +29,35 @@ public class ServerImplementation implements Server {
             System.out.println("Unable to start server: " + e.getMessage());
         }
         
-        this.updateUserThread = new Thread(new UpdateUsers(this.serverSocket));
-        
-        this.updateUserThread.start();
+        this.userSocketUpdate = new Thread(new ConnectionManager(this.serverSocket));
+        this.activeConversations = new ArrayList<>();
 
-        this.currentUsers = UpdateUsers.currentUsers;
-        this.userNetMap = UpdateUsers.userNetMap;
+        try {
+            this.userSocketUpdate.start();
+        } catch(IllegalThreadStateException e) {
+            e.printStackTrace();
+        }
+
+        this.userSockets = ConnectionManager.sockets;
+    }
+
+    public static User userExists(String userName) {
+        for(User user : activeUsers) {
+            if(user.getName().equals(userName)) {
+                return user;
+            }
+        }
+        return null;
     }
 
     public boolean startup() {
-        int initialusers = 
-        while()
+        int currentUserSize = ConnectionManager.userSize;
+        while (true) { 
+            if(ConnectionManager.userSize > currentUserSize) {
+                currentUserSize = ConnectionManager.userSize;
+                activeConversations.add(new ConversersationHandler(ConnectionManager.sockets.get(currentUserSize - 1)));
+                activeConversations.get(currentUserSize - 1).start();
+            }
+        }
     }
-    
 }
