8377d72f043af791ae9cb99660cfe244438c80c2
DY2718 <park1815@purdue.edu>
2024-11-17 06:19:58 -0500

    Add test cases for ServerImplementation, ClientSide, UpdateUsers, and GUIForApp

---
+++ tests/GUIForAppTest.java
@@ -0,0 +1,25 @@
+package tests;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import src.gui.GUIForApp;
+
+public class GUIForAppTest {
+    private GUIForApp gui;
+
+    @Before
+    public void setUp() {
+        gui = new GUIForApp();
+    }
+
+    @Test
+    public void testGUIForAppConstructor() {
+        assertNotNull("GUIForApp instance should be created", gui);
+    }
+
+    @Test
+    public void testTheGUIExitPressed() {
+        assertFalse("Exit should not be pressed initially", gui.theGUIExitPressed());
+    }
+}
