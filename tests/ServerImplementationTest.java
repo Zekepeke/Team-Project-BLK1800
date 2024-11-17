48ed9d8989d46e0983f6ad18399b9cb2f63bdff6
DY2718 <park1815@purdue.edu>
2024-11-17 06:25:32 -0500

    Update ServerImplementationTest.java

---
+++ tests/ServerImplementationTest.java
@@ -0,0 +1,34 @@
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import src.ServerImplementation;
+
+public class ServerImplementationTest {
+    private ServerImplementation server;
+
+    @Before
+    public void setUp() {
+        server = new ServerImplementation(8080);
+    }
+
+    @Test
+    public void testServerConstructor() {
+        assertNotNull("Server instance should be created", server);
+    }
+
+    @Test
+    public void testStartup() {
+        boolean result = server.startup();
+        assertTrue("Server should start up successfully", result);
+    }
+
+    @Test
+    public void testCurrentUsers() {
+        assertNotNull("Current users list should be initialized", server.currentUsers);
+    }
+
+    @Test
+    public void testUserNetMap() {
+        assertNotNull("User network map should be initialized", server.userNetMap);
+    }
+}
