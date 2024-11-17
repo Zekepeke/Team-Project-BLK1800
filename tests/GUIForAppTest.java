44083bfab79252ba5d77e998c9d3f21206dbf7ba
DY2718 <park1815@purdue.edu>
2024-11-17 07:51:45 -0500

    Update GUIForAppTest.java

--- tests/GUIForAppTest.java
+++ tests/GUIForAppTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 import src.gui.GUIForApp;
+import javax.swing.*;
 
 public class GUIForAppTest {
     private GUIForApp gui;
@@ -21,5 +22,27 @@ public class GUIForAppTest {
     @Test
     public void testTheGUIExitPressed() {
         assertFalse("Exit should not be pressed initially", gui.theGUIExitPressed());
+    }
+
+    @Test
+    public void testSendButtonSendsMessageToServerWithMock() {
+        MockServer mockServer = new MockServer();
+        gui.setServer(mockServer); 
+        JTextField messageField = gui.getMessageField();
+        JButton sendButton = gui.getSendButton();
+
+        messageField.setText("Test Message");
+        sendButton.doClick();
+
+        assertEquals("Message sent to server", "Test Message", mockServer.getLastReceivedMessage());
+    }
+
+    @Test
+    public void testGUIStateChangeOnConnection() {
+        gui.updateConnectionStatus(true); 
+        assertTrue("Send button should be enabled", gui.getSendButton().isEnabled());
+
+        gui.updateConnectionStatus(false); 
+        assertFalse("Send button should be disabled", gui.getSendButton().isEnabled());
     }
 }
