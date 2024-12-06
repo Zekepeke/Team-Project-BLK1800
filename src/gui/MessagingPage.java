26f3b4e62bb292f66c3b95a22f0d0168f33b7ee4
Yliu-101 <kailiu989@gmail.com>
2024-12-06 18:49:33 -0500

    added interface!

--- src/gui/MessagingPage.java
+++ src/gui/MessagingPage.java
@@ -1,5 +1,6 @@
 package src.gui; //package src.gui.pages.messaging;
 
+import interfaces.MessagingPageable;
 import src.ConversationReader;
 import src.Message;
 import src.User;
@@ -13,7 +14,7 @@ import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class MessagingPage extends JPanel {
+public class MessagingPage extends JPanel implements MessagingPageable {
     private User currentUser;
     private User chatPartner;
     private ClientSide client;
